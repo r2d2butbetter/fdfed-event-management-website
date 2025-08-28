f837aec6faef2bf2e47c1b899cb91faf51690413
Shriansh Jain <shriansh.j23@iiits.in>
2025-08-28 13:50:52 +0530

    Add material UI setup and check it's working

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from '../components/navbar';
+import ButtonUsage from '../components/button';
 
 function Home() {
     return (
         <div>
+            <Navbar />
             <h1>Home</h1>
             <p>Welcome to the Home page!</p>
+            <ButtonUsage />
         </div>
     );
 }
