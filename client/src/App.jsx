acb84d5ae4aadb11a77d8ed3a06eaa3a4ef80b32
koushik4163 <koushikreddy0378@gmail.com>
2025-09-08 11:02:22 +0530

    added contactus toApp.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
-import './App.css';
+import ContactUs from './pages/ContactUs';
+import './App.css'
 
 function App() {
   return (
     <>
       <nav>
-        <Link to="/">Home</Link> | <Link to="/about-us">About Us</Link>
+        <Link to="/">Home</Link> | <Link to="/about-us">About Us</Link> | <Link to="/contact-us">Contact Us</Link>
       </nav>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about-us' element={<AboutUs />} />
+        <Route path='/contact-us' element={<ContactUs />} />
       </Routes>
     </>
   );
