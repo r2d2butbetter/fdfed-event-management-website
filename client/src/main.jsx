f8bfe73269000f9ca45e1b64c47d6615416f11c0
Shriansh Jain <shriansh.j23@iiits.in>
2025-12-01 18:58:47 +0530

    Temp Commit- Shriansh

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext.jsx'
 import App from './App.jsx'
 import { CssBaseline } from '@mui/material'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <CssBaseline />
-        <App />
-      </AuthProvider>
+      <CssBaseline />
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
